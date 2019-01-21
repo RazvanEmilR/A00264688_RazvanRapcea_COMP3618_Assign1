f74150a3f972f468cfbf6e299ff3b98ae2ef2f65
RazvanR <39808280+RazvanEmilR@users.noreply.github.com>
2019-01-20 23:08:25 -0800

    Add student - ok
    Add student, tested

---
+++ CourseEnrollmentAppConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using CourseEnrollmentApp.Data;
+using CourseEnrollmentApp.Domain;
+using System;
+
+namespace CourseEnrollmentAppConsole
+{
+    class Program
+    {
+        private static CourseEnrollmentContext _context = new CourseEnrollmentContext(); 
+
+        static void Main(string[] args)
+        {
+            CreateStudent();
+            Console.ReadKey();
+        }
+
+        private static void CreateStudent()
+        {
+            // DateTime enrollmentDate = new DateTime(2019, 0, 1);
+            DateTime enrollmentDate = DateTime.Now;
+            var student = new Student { FirstName = "John", LastName = "Doe", MiddleName = "Smith", EnrollmentDate = enrollmentDate, Enrollments = null, Id = "A000000001" };
+
+            try
+            {
+                _context.Students.Add(student);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
